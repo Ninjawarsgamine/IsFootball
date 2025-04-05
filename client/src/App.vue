09bdf9fdf59cce0755f4b07d52442b37bb0de659
Ninjawarsgamine <robertomr555@gmail.com>
2025-04-05 23:23:37 +0200

    Se modifica App.vue para que funcionen las rutas de Vue Router.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,3 @@
 <template>
-  <TestComponent/>
+  <router-view />
 </template>
-
-<script setup>
-import TestComponent from './components/TestComponent.vue';
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
