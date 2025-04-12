2663b08cf1ba79d48be6b9ff7b5c80e8737c2506
Ninjawarsgamine <robertomr555@gmail.com>
2025-04-12 14:32:22 +0200

    Se han añadido las funciones getCompetitionById en CompetitionService y CompetitionController.

--- server/src/main/java/com/isfootball/controller/CompetitionController.java
+++ server/src/main/java/com/isfootball/controller/CompetitionController.java
@@ -1,8 +1,45 @@
 package com.isfootball.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.isfootball.model.Competition;
+import com.isfootball.service.CompetitionService;
+
+@RestController
 public class CompetitionController {
 	
+	private final CompetitionService competitionService;
+	
+    /**
+     * Constructor del controlador donde Spring inyecta automáticamente el servicio.
+     * 
+     * @param competitionService Es el servicio.
+     */
+    @Autowired
+	public CompetitionController(CompetitionService competitionService) {
+	        this.competitionService = competitionService;
+	 }
+	
+    /**
+	 * Endpoint REST para obtener los datos de una competición por su id.
+	 * @param id El id de la competición.
+	 * @return Un objeto "Competition" con los datos de cada competición.
+	 */
+	@GetMapping("/api/competition/{id}")
+	public Competition getCompetitionById(String id) {
+		return competitionService.getCompetitionById(Integer.valueOf(id));
+	}	
+    
+	/**
+	 * Endpoint REST para obtener los datos de una competición por su nombre.
+	 * @param name El nombre de la competición.
+	 * @return Un objeto "Competition" con los datos de cada competición.
+	 */
+	@GetMapping("/api/competition/{name}")
+	public Competition getCompetitionByName(@PathVariable String name) {
+		return competitionService.getCompetitionByName(name);
+	}
 }
--- server/src/main/java/com/isfootball/service/CompetitionService.java
+++ server/src/main/java/com/isfootball/service/CompetitionService.java
@@ -35,7 +35,58 @@ public class CompetitionService {
 		this.restTemplate = new RestTemplate();
 		this.objectMapper = new ObjectMapper();
 	}
+	
+	/**
+	 * Función que devuelve todos los datos de una competición en base a su ID.
+	 * 
+	 * @param competitionId Id de la competición.
+	 * @return Los datos de la competición con el ID especificado.
+	 */
+	public Competition getCompetitionById(Integer competitionId) {
+		if(competitionId==null) {
+			throw new IllegalArgumentException("The competition id is null.");
+		}
+		if(!(competitionId instanceof Integer)) {
+			throw new IllegalArgumentException("The competition id is not a Integer.");
+		}
+		String url="https://"+apiHost+"/leagues?id="+competitionId+"+&season="+season;
+		HttpHeaders headers=new HttpHeaders();
+		headers.set("x-rapidapi-key", apiKey);
+	    headers.set("x-rapidapi-host", apiHost);
+	    
+	    HttpEntity<String> entity = new HttpEntity<>(headers);
+	    ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+	    
+	    String jsonResponse=response.getBody();
+	    
+	    try {
+	    	JsonNode responseBody=objectMapper.readTree(jsonResponse);
+			JsonNode competitionAllData=responseBody.path("response").get(0);
 
+			Competition competition=new Competition();
+
+			JsonNode competitionInfo=competitionAllData.path("league");
+			competition.setId(competitionInfo.get("id").asInt());
+			competition.setName(competitionInfo.get("name").asText());
+			competition.setType(competitionInfo.get("type").asText());
+			competition.setLogo(competitionInfo.get("logo").asText());
+
+			JsonNode competitionCountry=competitionAllData.path("country");
+			Country country=new Country();
+			country.setName(competitionCountry.path("name").asText());
+			country.setCode(competitionCountry.path("code").asText());
+			country.setFlag(competitionCountry.path("flag").asText());
+			
+			JsonNode competitionSeason=competitionAllData.path("season");
+			competition.setSeason(competitionSeason.path("year").asInt());
+			
+	    	return competition;
+	    }catch(Exception e) {
+			e.printStackTrace();
+	    	return null;
+	    }
+	}
+	
 	/**
 	 * Función que devuelve los datos de una competición por su nombre.
 	 * 
